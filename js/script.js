84df3ddf5ab984a74972929943d7d1622ef91aa5
AlicePlayez <aliceplayez@gmail.com>
2024-06-13 17:33:29 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+var typed = new Typed(".typing" , {
+    strings:["", "A Mobile Application Developer", "A Web Developer", "An Instructor"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop:true
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
